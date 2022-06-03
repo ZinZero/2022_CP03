e14976c732e91e1493d9410d367070a3ae8bf73b
ZinZero <realbloom0122@gmail.com>
2022-06-03 22:51:50 +0900

    220603

--- Exercise/midRange/coboCP1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    printf("C is interesting.\n");
-    printf("Practice is the royal road to C programming.\n");
-
-    return 0;
-}
--- MidExam/Q7.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int iarr[4] = {10,20,30,40};
-	int *ip = iarr;
-
-	printf("%d %d\n",sizeof(iarr),sizeof(ip));
-}
